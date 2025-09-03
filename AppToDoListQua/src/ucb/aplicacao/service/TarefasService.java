5f3e17c61d8ddbcb248a82aa2bf58bc73ed8bdf3
Eduardo Gonçalves <eduardouc388@gmail.com>
2025-09-03 10:52:10 -0300

    Update

--- AppToDoListQua/src/ucb/aplicacao/service/TarefasService.java
+++ AppToDoListQua/src/ucb/aplicacao/service/TarefasService.java
@@ -1,4 +1,21 @@
 package ucb.aplicacao.service;
 
+import ucb.aplicacao.model.Tarefas;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TarefasService {
+    private List<Tarefas> tarefas = new ArrayList<>();
+    private Long contadorId = 1L;
+
+    public Tarefas criarTarefa(String titulo, String descrição) {
+        Tarefas tarefa = new Tarefas(contadorId++, titulo, descrição, false);
+        tarefas.add(tarefa);
+        return tarefa;
+    }
+
+    public List<Tarefas> getTarefas() {
+        return tarefas;
+    }
 }
